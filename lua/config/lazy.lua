9003f3af5337120b7335399710784a5fc5e5116e
Shrimp <2164516644@qq.com>
2024-12-22 16:22:52 +0800

    update localleader

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,8 +15,8 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.mapleader = " " -- global leader
+vim.g.maplocalleader = " " -- local leader
 
 -- Setup lazy.nvim
 require("lazy").setup({
