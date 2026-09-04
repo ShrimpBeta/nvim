c92220f558f14c927ca2cfe1c7c52f000c34e735
alistairx256 <81841997+alistairx256@users.noreply.github.com>
2026-09-04 16:07:39 +0800

    Update keymaps.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,3 +9,19 @@ keymap.set("n", "<leader>tt", ":NvimTreeToggle<CR>")
 keymap.set("n", "<C-i>", ":bnext<CR>")
 keymap.set("n", "<C-n>", ":bprevious<CR>")
 keymap.set("n", "<C-e>", ":bdelete<CR>")
+
+-- telescope
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {
+  desc = "Find files",
+})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
+  desc = "Live grep",
+})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {
+  desc = "Buffers",
+})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {
+  desc = "Help tags",
+})
