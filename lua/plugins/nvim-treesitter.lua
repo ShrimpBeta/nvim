d2ddfdaf4ed0d65847dbf6f764c1340f52fc7ddd
alistair <2164516644@qq.com>
2026-09-06 10:48:18 +0800

    enable nvim-treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  enabled = false,
+  -- enabled = false,
   build = ":TSUpdate",
   config = function()
     local treesitter = require("nvim-treesitter.configs")
